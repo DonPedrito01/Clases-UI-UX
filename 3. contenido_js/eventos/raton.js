c7ce7526dea4a18e53ad06f424b18b2ea3c8ed51
DonPedrito01 <probayo@uniempresarial.edu.co>
2025-02-10 17:45:42 -0500

    Funciones click
    Lo que paso en esta clase fue que en la terminal se puede observar cuando el puntero del mouse esta encima de un parrafo o cuantas veces pasa por encima, ademas de que se puede observar el tecleado con 3 funciones, cuando aprieta la tecla, cuando la suelta y otra que no recuerdo bien

---
+++ 3. contenido_js/eventos/raton.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//metodo mediante envento
+
+const capturaClick = () =>{
+    alert('Evento Click');
+}
+
+const capturaMouseOver = () =>{
+    console.log('Evento Mouse Over');
+}
+
+const capturaMove = () =>{
+    console.log('Evento Move');
+}
---
+++ 3. contenido_js/eventos/teclado.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const funcionInput = () => {
+    console.log('tecleando');
+}
